b8fd7fd50dafbb3e7f6b511be6024a51fd16ff8d
浮田岳洋 <ukita1022@ukitatakeshiyounoMacBook-Pro.local>
2020-04-12 18:41:28 +0900

    add rspec

--- README.md
+++ README.md
@@ -1,24 +1,11 @@
-# README
+# cms01 
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## 説明
 
-Things you may want to cover:
 
-* Ruby version
 
-* System dependencies
+## 推奨されるシステム環境
 
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+*
+*
+*
--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,17 @@ module Cms01
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path +=
+      Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.test_framework :rspec
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    end
   end
 end
